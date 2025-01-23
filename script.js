5fdbfdbb0f798e7aee2081a4fa66e7c1414f5176
shiva <shiva@nimapinfotech.com>
2025-01-23 18:07:35 +0530

    Profile detail view done

--- script.js
+++ script.js
@@ -336,11 +336,181 @@ OrgChart.templates.base = Object.assign({}, OrgChart.templates.base, {
   <image xlink:href="./assets/stats.png" x="210" y="8" width="20" height="20"></image></g>`,
 });
 
-OrgChart.templates.base.ripple = {
-  radius: 100,
-  color: "#ffffff",
-  rect: null,
+let editForm = function () {};
+
+editForm.prototype.init = function (obj) {
+  this.chart = obj;
 };
+
+editForm.prototype.show = function (nodeId) {
+  this.hide();
+  let node = this.chart.get(nodeId);
+
+  // console.log("node", node);
+  let editHtml = `
+        <div>
+         <div class="modal-actions">
+          <i class="fa-solid fa-up-down-left-right modal-actions-icon"></i>
+          <i class="fa-solid fa-up-right-and-down-left-from-center modal-actions-icon"></i>
+          <i class="fa-solid fa-circle-xmark modal-actions-icon cross-icon"></i>
+         </div>
+         <div class="profile-details">
+          <div class="profile-img">
+            <img class="img" src="${node?.img}" />
+            <div class="img-texts">
+              <div class="text">
+                <p>
+                  <span>${node.rating}</span>
+                  <i class="fa-solid fa-star star-icon"></i>
+                </p>
+                <p>${node.name}</p>
+                <p>
+                  <label>${node.position}</label>
+                  <label>${node.location}</label>
+                </p>
+              </div>
+              <div><i class="fa-solid fa-ellipsis-vertical"></i></div>
+            </div>
+            <div class="stats-container">
+              <button class="stats-btn">
+                <i class="fa-solid fa-chart-pie stats-icon"></i>
+                <span>Statistics</span>
+              </button>
+            </div>
+          </div>
+          <div class="profile-details">
+            <h2>About</h2>
+            <div class="about-details">
+              <div>
+                <label>Company</label>
+                <p>HBL pvt. ltd</p>
+              </div>
+              <div>
+                <label>Department</label>
+                <p class="department">
+                  <img src="./assets/department_icon.png" alt="Department_Icon" class="department-icon"/>
+                  <label>${node.department}</label>
+                </p>
+              </div>
+              <div>
+                <label>Reporting to</label>
+                <p>Jaydeep Das</p>
+              </div>
+              <div>
+                <label>Team size</label>
+                <p>18</p>
+              </div>
+              <div>
+                <label>Direct reporting</label>
+                <p>Gaurav Chawla</p>
+              </div>
+              <div>
+                <label>Contact Info.</label>
+                <p>+93 123456789</p>
+              </div>
+              <div>
+                <label>DOJ</label>
+                <p>1 Jan,2025</p>
+              </div>
+              <div>
+                <label>Skills & Certification</label>
+                <p>Master in Excel</p>
+              </div>
+          </div>
+          <div>
+            <p>Team Members</p>
+            <div class="team-members">
+              <img src="./assets/teams_icon.png" class="teams-icon" alt="Teams Icon"/>
+              <div class="teamMembers-lists">
+              ${[...Array(6)].map((_) => {
+                return `<img class="teamsMember-img " src="https://cdn.balkan.app/shared/2.jpg"/>`;
+              })}
+              </div>
+              <span>+10</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr/>
+      <section class="kra-kpi-container">
+        <div class="kraKpi-card kra-card">
+            <h2>KRA</h2>
+            <div class="kraKpi-actions">
+              <button>
+                <img src="./assets/dots.png"/>
+                <span>Move one by one</span>
+              </button>
+              <button>
+                <input type="checkbox"/>
+                <span>Move one by one</span>
+              </button>
+            </div>
+            <div class="kra-items">
+             ${[...Array(5)]
+               .map((_, id) => {
+                 return `<div key=${id} class="kra-item">
+                <img src="./assets/dots.png" class="dot-icon"/>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+              </div>`;
+               })
+               .join("")}
+            </div>
+            
+        </div>
+        <div class="kraKpi-card kpi-card">
+            <h2>KPI</h2>
+            <div class="kraKpi-actions">
+              <button>
+                <img src="./assets/dots.png"/>
+                <span>Move one by one</span>
+              </button>
+              <button>
+                <input type="checkbox"/>
+                <span>Move one by one</span>
+              </button>
+            </div>
+            <div class="kpi-items">
+             ${[...Array(5)]
+               .map((_, id) => {
+                 return `<div key=${id} class="kpi-item">
+                <input type="checkbox" class="kpi-checkbox"/>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+              </div>`;
+               })
+               .join("")}
+            </div>
+           
+        </div>
+      </section>
+    </div>`;
+
+  let div = document.createElement("div");
+  div.id = "edit-form";
+  div.innerHTML = editHtml;
+  div.classList.add("employee-details");
+  this.chart.element.appendChild(div);
+
+  OrgChart.animate(
+    div,
+    { opacity: 0, right: -100 },
+    { opacity: 1, right: 10 },
+    300,
+    OrgChart.anim.outSin
+  );
+};
+
+editForm.prototype.hide = function (showldUpdateTheNode) {
+  let form = this.chart.element.querySelector("#edit-form");
+  if (form) {
+    form.parentNode.removeChild(form);
+  }
+};
+
+// OrgChart.templates.base.ripple = {
+//   radius: 100,
+//   color: "#ffffff",
+//   rect: null,
+// };
 
 OrgChart.templates.base.editFormHeaderColor = "#1A2024";
 
@@ -348,6 +518,7 @@ var chart = new OrgChart(document.getElementById("tree"), {
   template: "base",
   mouseScrool: OrgChart.action.none,
   enableDragDrop: true,
+  editUI: new editForm(),
   editForm: {
     readOnly: true,
     buttons: {
@@ -381,6 +552,10 @@ const treeContainer = document.getElementById("tree");
 chart.on("click", function (sender, args) {
   const clickedNode = chart.get(args.node.id);
   console.log("Clicked Node Data:", clickedNode);
+});
+
+chart.on("init", function (sender) {
+  sender.editUI.show(0);
 });
 
 chart.on("update", function (sender, oldNode, newNode) {
